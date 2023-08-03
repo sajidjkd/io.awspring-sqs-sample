6b5922abf9209c7c669c3d30c474db2161fdc2bb
Sajed <sajid.jkd@gmail.com>
2023-08-03 22:43:53 +0530

    updated io.awspring.cloud to 2.4.1 and Spring 2.7.8

--- src/main/java/com/awsspringboot/sample/messaging/MessageReceiver.java
+++ src/main/java/com/awsspringboot/sample/messaging/MessageReceiver.java
@@ -19,7 +19,7 @@ public class MessageReceiver {
     public void receiveStringMessage(final String message,
                                      @Header("SenderId") String senderId) {
         log.info("message received {} {}",senderId,message);
-    }
+    }/*
 
     @SqsListener(value = "http://0.0.0.0:4566/000000000000/order-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS )
     public void receiveStringMessage(final String message) {
@@ -35,12 +35,6 @@ public class MessageReceiver {
     @SqsListener(value = "http://0.0.0.0:4566/000000000000/order-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS )
     public void receiveObjectMessage(final Event message) {
         log.info("object message received {} {}",message);
-    }
+    }*/
 
-    @SqsListener(value = "http://localhost:4566/000000000000/order-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void receiveS3Event(S3EventNotification s3EventNotificationRecord) {
-        S3EventNotification.S3Entity s3Entity = s3EventNotificationRecord.getRecords().get(0).getS3();
-        String objectKey = s3Entity.getObject().getKey();
-        log.info("s3 event::objectKey:: {}",objectKey);
-    }
 }
